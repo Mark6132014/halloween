7988fad976405db3ffeb983a5f91b2fc790891fb
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-09-22 10:11:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-alert("Welcome to the Halloween Website!\nWhere you can see something about Halloween, fun facts, and Did you know? Facts!");
 document.querySelector(".game1").addEventListener("click", () => {
     let GameClient = window.open("https://mark6132014.github.io/catchMe", "popup", "height=600,width=800");
     GameClient.focus();
